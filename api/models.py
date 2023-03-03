bf2707b872277c7e02b8cc5b3bef49897ad979b8
Agustin Fricke <agustfricke@protonmail.com>
2023-03-03 08:55:52 -0300

    users

--- api/models.py
+++ api/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class User(models.Model):
     user = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.user
+
 class Blog(models.Model):
     title = models.CharField(max_length=50)
     body = models.CharField(max_length=50)
--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,7 @@ from rest_framework import routers
 from . views import BlogViewSet, UserViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts/', BlogViewSet, 'blogs')
-router.register(r'users/', UserViewSet, 'users')
+router.register(r'posts', BlogViewSet, 'blogs')
+router.register(r'users', UserViewSet, 'users')
 
 urlpatterns = router.urls
-
